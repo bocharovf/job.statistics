321f572105e015ab5a3a2baa9ab477926c75fc87
Bocharov Filipp <bocharovf@gmail.com>
2015-11-09 00:00:13 +0300

    Add angularjs
    Add angularjs
    Change layout a bit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+angular.module('hhStat', ['angular-jqcloud'])
+    .controller('TagCloudCtrl', function($scope) {
+         $scope.words = [{text: "Lorem", weight: 13},
+						  {text: "Ipsum", weight: 10.5},
+						  {text: "Dolor", weight: 9.4},
+						  {text: "Sit", weight: 8},
+						  {text: "Amet", weight: 6.2},
+						  {text: "Consectetur", weight: 5},
+						  {text: "Adipiscing", weight: 5}];
+    });
